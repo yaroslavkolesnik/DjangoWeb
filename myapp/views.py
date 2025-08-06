225967b018cabd66d1520a73393d4fae5c5743f5
Yaroslav Kolisnuk <02.07.2005k@gmail.com>
2025-08-06 13:04:58 +0300

    add breadcrumbs and button-to-top

--- myapp/views.py
+++ myapp/views.py
@@ -1,14 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 # Create your views here.
 def index(request):
     return render(request, 'myapp/index.html')
 def about(request):
-    return render(request, 'myapp/about.html')
+    breadcrumbs = [
+        {'name': _('Главная'), 'url': reverse('home')},
+        {'name': _('О нас'), 'url': reverse('about')},
+    ]
+    context = {'breadcrumbs': breadcrumbs}
+    return render(request, 'myapp/about.html', context)
 def contact(request):
-    return render(request, 'myapp/contact.html')
+    breadcrumbs = [
+        {'name': _('Главная'), 'url': reverse('home')},
+        {'name': _('Контакты'), 'url': reverse('contact')},
+    ]
+    context = {'breadcrumbs': breadcrumbs}
+    return render(request, 'myapp/contact.html', context)
+
 def show(request):
-    return render(request, 'myapp/show.html')
+    breadcrumbs = [
+        {'name': _('Главная'), 'url': reverse('home')},
+        {'name': _('Товар'), 'url': reverse('show')},
+    ]
+    context = {'breadcrumbs': breadcrumbs}
+    return render(request, 'myapp/show.html', context)
 def favorites(request):
     return render(request, 'myapp/favorites.html')
--- static/js/main.js
+++ static/js/main.js
@@ -1,2 +1,50 @@
 console.log('JavaScript подключен');
 
+const menuToggle = document.getElementById('menu-toggle');
+const menuClose = document.getElementById('menu-close');
+const mobileMenu = document.getElementById('mobile-menu');
+
+menuToggle.addEventListener('click', () => {
+   mobileMenu.classList.remove('-translate-x-full');
+});
+
+menuClose.addEventListener('click', () => {
+  mobileMenu.classList.add('-translate-x-full');
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    const backToTopButton = document.createElement("a");
+    backToTopButton.href = "#";
+    backToTopButton.id = "ui-to-top-custom";
+    backToTopButton.className = "ui-to-top fa fa-angle-up"; // Ваши классы, включая Font Awesome
+    backToTopButton.setAttribute("aria-label", "Вернуться наверх");
+
+    document.body.appendChild(backToTopButton);
+
+    const button = document.getElementById("ui-to-top-custom");
+
+    const minScroll = 500;
+    const scrollSpeed = 800;
+
+    window.addEventListener("scroll", function() {
+        if (window.scrollY > minScroll) {
+            button.classList.add("active");
+        } else {
+            button.classList.remove("active");
+        }
+    });
+
+    button.addEventListener("click", function(e) {
+        e.preventDefault();
+
+        if (document.documentElement.scrollTop === 0 && document.body.scrollTop === 0) {
+            return;
+        }
+
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
+});
